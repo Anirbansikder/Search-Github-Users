c87c5e626884169e4d781c1a9a91eb0a11223aa1
Anirban Sikder <sikder.anirbansikder.sikder@gmail.com>
2022-04-13 23:59:05 +0530

    Finshed Chart Components

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -31,46 +31,26 @@ const Repos = () => {
   }).map((item) => {
     return {...item,value : item.stars}
   }).slice(0,5);
-  const chartData = [
-    {
-      label: "HTML",
-      value: "13"
-    },
-    {
-      label: "CSS",
-      value: "23"
-    },
-    {
-      label: "JS",
-      value: "50"
-    },
-    {
-      label: "Iran",
-      value: "140"
-    },
-    {
-      label: "Russia",
-      value: "115"
-    },
-    {
-      label: "UAE",
-      value: "100"
-    },
-    {
-      label: "US",
-      value: "30"
-    },
-    {
-      label: "China",
-      value: "30"
-    }
-  ];
+
+  // stars  and forks
+
+  let {stars , forks} = repos.reduce ((total,item) => {
+    const {stargazers_count,name,forks} = item;
+    total.stars[stargazers_count] = {label : name , value : stargazers_count};
+    total.forks[forks] = {label : name , value : forks};
+    return total
+  },{
+    stars : {} , forks : {}
+  })
+
+  stars = Object.values(stars).slice(-5).reverse();
+  forks = Object.values(forks).slice(-5).reverse();
   return <section className='section'>
     <Wrapper className='section-center'>
       <Pie3D data={mostUsed}/>
-      <Column3D data={chartData}/>
+      <Column3D data={stars}/>
       <Doughnut2D data = {mostPopular}/>
-      <Bar3D data = {chartData} />
+      <Bar3D data = {forks} />
     </Wrapper>
   </section>
 };
