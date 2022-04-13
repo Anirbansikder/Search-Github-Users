c64d4aac56718d5a0375393b2d8b37353577eaa5
Anirban Sikder <sikder.anirbansikder.sikder@gmail.com>
2022-04-13 23:11:09 +0530

    Added Pie Chart

--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -1,7 +1,33 @@
-import React from 'react';
+// STEP 1 - Include Dependencies
+// Include react
+import React from "react";
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Chart from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
 
-const Pie3D = () => {
-  return <div>chart</div>;
-};
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
 
-export default Pie3D;
+const ChartComponent = ({data}) => {
+  const chartConfigs = {
+    type: "pie3d", // The chart type
+    width: "400", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption : "Languages",
+        theme : "Fusion",
+        decimals : 0,
+        pieRadius : "50%",
+      },
+      // Chart Data
+      data,
+    }
+  };
+  return (<ReactFC {...chartConfigs} />);
+}
+
+export default ChartComponent;
+
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -5,6 +5,25 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 
 const Repos = () => {
   const {repos} = React.useContext(GithubContext);
+  let languages = repos.reduce((total,item) => {
+    const { language } = item;
+    if(!language) return total;
+    if(!total[language]){
+      total[language] = {
+        label : language,
+        value : 1
+      };
+    } else {
+      total[language] = {
+        ...total[language],
+        value : total[language].value + 1
+      };
+    }
+    return total;
+  },{});
+  languages = Object.values(languages).sort((a,b) => {
+    return b.value - a.value;
+  }).slice(0,5);
   const chartData = [
     {
       label: "HTML",
@@ -41,7 +60,8 @@ const Repos = () => {
   ];
   return <section className='section'>
     <Wrapper className='section-center'>
-      <ExampleChart data={chartData}/>
+      {/* <ExampleChart data={chartData}/> */}
+      <Pie3D data={languages}/>
     </Wrapper>
   </section>
 };
